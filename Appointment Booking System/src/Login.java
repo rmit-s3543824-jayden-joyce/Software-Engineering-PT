d53a41f7801ad586f744c04ffde570496491eb73
rmit-s3543824-jayden-joyce <s3543824@student.rmit.edu.au>
2017-03-25 18:37:34 +1100

    Update Login.java

--- Appointment Booking System/src/Login.java
+++ Appointment Booking System/src/Login.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 import java.io.*;
 import java.lang.reflect.Array;
@@ -33,39 +32,3 @@ public class Login
 		return false;
 	}
 }
-
-import java.util.Scanner;
-import java.io.*;
-import java.lang.reflect.Array;
-
-public class Login
-{	
-	static String delim = " ";
-	
-	public boolean verifyLoginDetails(String username, String password, String userType) throws IOException {
-		
-		String[] words;
-		String line = "";
-		FileReader fr = new FileReader(userType);
-		BufferedReader reader = new BufferedReader(fr);
-		
-		while((line = reader.readLine()) != null)
-		{
-			words = line.split(delim);
-			
-			if((words[0]).equals(username))
-      {
-				if((words[1]).equals(password))
-				{
-					reader.close();
-					return true;
-				}
-			}
-		}
-		
-		System.out.println("Username or password not found\n");
-		reader.close();
-		return false;
-	}
-}
-
