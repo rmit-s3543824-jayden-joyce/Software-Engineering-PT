748e6cab7710e17ae3a2fc1cc160510c329967dd
rmit-s3543824-jayden-joyce <s3543824@student.rmit.edu.au>
2017-03-28 14:55:31 +1100

    Login class
    - Moved respective menu code into login class
    - Solved scanner problem (closed scanner too early)

--- Appointment Booking System/src/Login.java
+++ Appointment Booking System/src/Login.java
@@ -1,18 +1,105 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Login
 {	
 	static String delim = "\\|";
+	static String customerList = "customerList.txt";
+	static String businessOwnerList = "businessOwnerList.txt";
 	
-	public boolean verifyLoginDetails(String username, String password, String userType) throws IOException {
+	public static void login() throws IOException
+	{
+		String userType, Username;
+		char passwordArray[];
+		boolean loginOption = true;
+		Console console = System.console();	
+		Scanner consoleReader = new Scanner(System.in);
 		
+		//Establishes whether user is a Customer or Business Owner
+		userTypeSelection();
+		userType = consoleReader.next();
+		
+		//Checks for invalid entries
+		if(!userType.equals("1") && !userType.equals("2"))
+		{
+			System.out.println("Invalid input.");
+			return;
+		}
+		
+		do
+		{
+			heading(userType);
+			
+			System.out.println("Username:");
+			System.out.print(">>> ");
+			Username = consoleReader.next();
+			System.out.println("Password:");
+			System.out.print(">>> ");
+			//console.readPassword has built-in password masking
+			passwordArray = console.readPassword();
+			
+			//Checks for invalid entries
+			if(passwordArray.length == 0 || Username.isEmpty())
+			{
+				System.out.println("Username or password in incorrect.\n");
+			}
+			else
+			{
+				loginOption = false;
+			}
+		}while(loginOption);
+		
+		//Depending on user type, decides which file to read from
+		if(userType.equals("1"))
+		{
+			if(verifyLoginDetails(Username, new String(passwordArray), customerList) == false)
+			{
+				return;
+			}
+		}
+		else
+		{
+			if(verifyLoginDetails(Username, new String(passwordArray), businessOwnerList) == false)
+			{
+				return;
+			}
+		}
+		
+		System.out.println("Login successful!\n");
+	}
+	
+	public static void userTypeSelection()
+	{
+		System.out.println("\nSelect user-type:");
+		System.out.println("1. Customer");
+		System.out.println("2. Business Owner");
+		System.out.print(">>> ");
+	}
+	
+	public static void heading(String x)
+	{
+		if(x.equals("1"))
+		{
+			System.out.println("\nLogin (Customer)");
+		}
+		else
+		{
+			System.out.println("\nLogin (Business Owner)");
+		}
+		System.out.println("------------------------");
+	}
+
+	public static boolean verifyLoginDetails(String username, String password, String userType) throws IOException
+	{
 		String[] words;
 		String line = "";
 		FileReader fr = new FileReader(userType);
 		BufferedReader reader = new BufferedReader(fr);
 		
+		//Reads each line from respective text file
 		while((line = reader.readLine()) != null)
 		{
+			//Splits up words in the line using delim
 			words = line.split(delim);
 			
 			if((words[0]).equals(username))
--- Appointment Booking System/src/Menu.java
+++ Appointment Booking System/src/Menu.java
@@ -1,99 +1,29 @@
 import java.io.IOException;
-import java.io.Console;
 import java.util.Scanner;
 
 public class Menu {
 	
-	static String customerList = "customerList.txt";
-	static String businessOwnerList = "businessOwnerList.txt";
-	
 	public static void main(String[] args) throws IOException
 	{
-		Login login = new Login();
 		Scanner sc = new Scanner(System.in);
 		boolean menu = true;
-		String Username;
-		char passwordArray[];
 		
 		do
 		{
-			menuScreen();
-			boolean loginOption = true;
-			Console console = System.console();			
+			menuScreen();		
 			String input = sc.next();
 			
 			switch(input)
 			{
 				case "1":
-					String userType;
-					
-					System.out.println("\nSelect user-type:");
-					System.out.println("1. Customer");
-					System.out.println("2. Business Owner");
-					System.out.print(">>> ");
-					
-					userType = sc.next();
-					
-					if(!userType.equals("1") && !userType.equals("2"))
-					{
-						System.out.println("Invalid input.");
-						break;
-					}
-					
-					do
-					{
-						if(userType.equals("1"))
-						{
-							System.out.println("\nLogin (Customer)");
-							System.out.println("------------------------");
-						}
-						else
-						{
-							System.out.println("\nLogin (Business Owner)");
-							System.out.println("------------------------");
-						}
-						
-						System.out.println("Username:");
-						System.out.print(">>> ");
-						Username = sc.next();
-						System.out.println("Password:");
-						System.out.print(">>> ");
-						passwordArray = console.readPassword();
-						
-						if(passwordArray.length == 0 || Username.isEmpty())
-						{
-							System.out.println("Username or password in incorrect.\n");
-						}
-						else
-						{
-							loginOption = false;
-						}
-						
-					}while(loginOption);
-					
-					if(userType.equals("1"))
-					{
-						if(login.verifyLoginDetails(Username, new String(passwordArray), customerList) == false)
-						{
-							break;
-						}
-					}
-					else
-					{
-						if(login.verifyLoginDetails(Username, new String(passwordArray), businessOwnerList) == false)
-						{
-							break;
-						}
-					}
-					
-					System.out.println("Login successful!\n");
+					Login.login();
 				break;
 				case "2":
 					Registration.register();
-					//register new customer/business owner
 				break;
 				case "3":
 					System.out.println("Exiting...");
+					sc.close();
 					menu = false;
 				break;
 				default:
@@ -101,8 +31,6 @@ public class Menu {
 				break;
 			}
 		}while(menu);
-		
-		sc.close();
 	}
 
 	public static void menuScreen()
