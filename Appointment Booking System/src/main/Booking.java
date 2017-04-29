20a0cdab02c7af8722d431d1de31af0f032f6db6
rmit-s3570599-jeremy-siaw <s3570599@student.rmit.edu.au>
2017-04-29 10:03:35 +1000

    Attribute added
    Service type added into booking

--- Appointment Booking System/src/main/Booking.java
+++ Appointment Booking System/src/main/Booking.java
@@ -10,27 +10,30 @@ public class Booking {
 	private String customerName; // The customer's name of the booking
 	private String employeeName; // The employee assigned to the booking
 	private String status;
+	private String serviceType;
 
 	// Constructor when there is no status given, to be used for new bookings
 	// which automatically sets the status of new bookings to "NEW"
 	public Booking(String year, String month, String date, String hour, String minute, String customerName,
-			String employeeName) {
+			String employeeName, String serviceType) {
 		this.date = LocalDate.of(Integer.valueOf(year), Integer.valueOf(month), Integer.valueOf(date));
 		this.time = LocalTime.of(Integer.valueOf(hour), Integer.valueOf(minute));
 		this.customerName = customerName;
 		this.employeeName = employeeName;
 		this.status = "NEW"; // New booking
+		this.serviceType = serviceType;
 		refreshStatus();
 	}
 
 	// If status is given, usually is used when reading booking from text file
 	public Booking(String status, String year, String month, String date, String hour, String minute,
-			String customerName, String employeeName) {
+			String customerName, String employeeName, String serviceType) {
 		this.date = LocalDate.of(Integer.valueOf(year), Integer.valueOf(month), Integer.valueOf(date));
 		this.time = LocalTime.of(Integer.valueOf(hour), Integer.valueOf(minute));
 		this.customerName = customerName;
 		this.employeeName = employeeName;
 		this.status = status;
+		this.serviceType = serviceType;
 		refreshStatus();
 	}
 
@@ -54,6 +57,10 @@ public class Booking {
 	public String getEmployeeName() {
 		return this.employeeName;
 	}
+	
+	public String getType(){
+		return this.serviceType;
+	}
 
 	// Mutators
 	public void setDate(LocalDate date) {
@@ -66,6 +73,10 @@ public class Booking {
 
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
+	}
+	
+	public void setType(String type){
+		this.serviceType = type;
 	}
 
 	// Checking the status of the booking with the date
@@ -104,6 +115,12 @@ public class Booking {
 		int employeeNameLength = this.employeeName.length();
 		System.out.print(this.employeeName);
 
+		for(int i = employeeNameLength; i < 12; i ++){
+			System.out.print(" ");
+		}
+		
+		System.out.print(this.serviceType);
+		
 		System.out.println();
 
 	}
